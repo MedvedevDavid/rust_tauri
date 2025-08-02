186522c61905f9d5f40746dd576ba84f1d8e12dc
MedveDev <medvedevdavid@gmail.com>
2025-08-02 17:26:26 +0200

    Connected drives are detected

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,18 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use sysinfo::Disks;
+
+fn detect_function() {
+    // We display all disks' information:
+    println!("=> disks:");
+    let disks = Disks::new_with_refreshed_list();
+    for disk in &disks {
+        println!("{:?}", disk.name());
+    }
+}
 fn main() {
-    tauri_app_lib::run()
+    let handle = std::thread::spawn(|| detect_function());
+    handle.join().unwrap();
+    tauri_app_lib::run();
 }
